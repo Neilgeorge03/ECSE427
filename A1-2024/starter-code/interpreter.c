d06262f5ca7de40d49586afa223dc7f9db972d77
Neilgeorge03 <49652125+Neilgeorge03@users.noreply.github.com>
2024-09-24 11:10:48 -0400

    Menu issues
    Trying to fix the spacing issue for the command descriptions

--- A1-2024/starter-code/interpreter.c
+++ A1-2024/starter-code/interpreter.c
@@ -92,13 +92,13 @@ int interpreter(char* command_args[], int args_size) {
 int help() {
 
     // note the literal tab characters here for alignment
-    char help_string[] =       "COMMAND                 DESCRIPTION\n\
-                                    help                   Displays all the commands\n\
-                                    quit                   Exits / terminates the shell with “Bye!”\n\
-                                    set VAR STRING         Assigns a value to shell memory\n\
-                                    print VAR              Displays the STRING assigned to VAR\n\
-                                    run SCRIPT.TXT         Executes the file SCRIPT.TXT";
-                                    printf("%s\n", help_string);
+    char help_string[] = "COMMAND			DESCRIPTION\n\
+help			Displays all the commands\n\
+quit			Exits / terminates the shell with “Bye!”\n\
+set VAR STRING		Assigns a value to shell memory\n\
+print VAR		Displays the STRING assigned to VAR\n\
+run SCRIPT.TXT		Executes the file SCRIPT.TXT\n ";
+    printf("%s\n", help_string);
     return 0;
 }
 
