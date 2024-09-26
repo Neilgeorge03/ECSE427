88d7bdc2d2b12110cd1e156d1209139ee72fa2ef
Neilgeorge03 <49652125+Neilgeorge03@users.noreply.github.com>
2024-09-26 12:28:38 -0400

    Fix spacing

--- A1-2024/starter-code/interpreter.c
+++ A1-2024/starter-code/interpreter.c
@@ -127,8 +127,8 @@ int help() {
 
     // note the literal tab characters here for alignment
     char help_string[] ="COMMAND                 DESCRIPTION\n \
-help                   Displays all the commands\n \
-quit                   Exits / terminates the shell with “Bye!”\n \
+help\t\t\tDisplays all the commands\n \
+quit\t\t\tExits / terminates the shell with “Bye!”\n \
 set VAR STRING         Assigns a value to shell memory\n \
 print VAR              Displays the STRING assigned to VAR\n \
 run SCRIPT.TXT         Executes the file SCRIPT.TXT\n ";
