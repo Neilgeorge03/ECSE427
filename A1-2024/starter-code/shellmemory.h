2e5877295f9f5cb6ab6d9dca382730f2ac3609ff
Neilgeorge03 <49652125+Neilgeorge03@users.noreply.github.com>
2024-11-27 14:42:16 -0500

    Forgot this

--- A1-2024/starter-code/shellmemory.h
+++ A1-2024/starter-code/shellmemory.h
@@ -6,7 +6,7 @@
 #define MEM_SIZE 1000
 #define MAX_SCRIPT_SIZE (MAX_USER_INPUT * 100)
 #define KEY_SIZE 100
-#define VARIABLE_STORE_SIZE 10
+#define VARIABLE_STORE_SIZE 30
 #define MAX_PROCESSES 100
 
 void mem_init();
@@ -15,12 +15,15 @@ int getFreeFrame();
 void loadPageFrameStore(int index, char* fileName);
 char *mem_get_value(char *var);
 void mem_set_value(char *var, char *value);
+char *variable_get_value(char *var);
+void variable_set_value(char *var, char *value);
 int loadScriptInMemory(FILE *fp, int pid);
 int clearMemory(int pid, int length);
 int loadScriptSharedMemory(char *scriptName);
 int removeScriptSharedMemory(char *scriptName);
 char* getLine(int frameIndex, int offset);
 void deleteFrame(int frameIndex);
+int checkScriptLoaded(char *scriptName);
 
 struct memory_struct {
     char *var;
